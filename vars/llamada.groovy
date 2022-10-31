e85387547f7e115a6d42754951786c26c7197c11
jerileemolina <j.molina.velasco@accenture.com>
2022-10-31 16:47:34 +0100

    groovy added

--- vars/llamada.groovy
+++ vars/llamada.groovy
@@ -1,6 +1,5 @@
-def call (map config = [:]) {
-n1:"APP_JAVA-INT", v1: 0.0.5
-n2:"APP_JAVA-PRE", v2: 0.0.6
-n3:"APP_JAVA-PRO", v3: 0.0.7
-}
-
+def leer_archivo(release.yaml)
+archivo = open(release.yaml, «r»)
+for linea in archivo:
+print(linea)
+archivo.close()
