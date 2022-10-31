0aadf0487cba33e15b82cc7baea40f79622040ff
jerileemolina <j.molina.velasco@accenture.com>
2022-10-31 12:39:17 +0100

    llamada commit

--- vars/llamada.groovy
+++ vars/llamada.groovy
@@ -1,1 +1,5 @@
-def configVal = readYaml file: "release.yaml"
+def call (map config = [:]) {
+  1: [name:"APP_JAVA-INT", version: 0.0.5],
+  2: [name:"APP_JAVA-PRE", version: 0.0.6],
+  3: [name:"APP_JAVA-PRO", version: 0.0.7]
+}
